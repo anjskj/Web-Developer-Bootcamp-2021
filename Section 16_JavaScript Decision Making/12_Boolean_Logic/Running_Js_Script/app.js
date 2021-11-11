e4314462e120363249e3fe1ab3f3f1aa78560602
anjskj <45606242+anjskj@users.noreply.github.com>
2021-11-11 07:40:50 +0100

    Section 16

---
+++ Section 16_JavaScript Decision Making/12_Boolean_Logic/Running_Js_Script/app.js
@@ -0,0 +1,11 @@
+console.log("HELLO FROM OUR FIRST JS FILE!!!!");
+let total = 1 + 3;
+console.log("GOODBYE!");
+
+console.warn('WARNING!!');
+console.error('ERROR!');
+alert('Ooopsie daisy...');
+
+//user input, always entered as a string so use parseInt to change into an integer.
+let userInput = parseInt(prompt("Enter your age "));
+console.log(userInput);
